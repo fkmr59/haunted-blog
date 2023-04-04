3522d8508b747de342c74b5d8b121bc516826ac5
terumitt <terumitt@pepabo.com>
2023-04-04 12:36:52 +0900

    他のユーザーが編集できないようにしたがtestがpassしない

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,13 +3,15 @@
 class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
-  before_action :set_blog, only: %i[show edit update destroy]
+  before_action :set_blog, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
   end
 
-  def show; end
+  def show
+    @blog = Blog.find(params[:id])
+  end
 
   def new
     @blog = Blog.new
@@ -45,6 +47,7 @@ class BlogsController < ApplicationController
 
   def set_blog
     @blog = Blog.find(params[:id])
+    redirect_to(blogs_path) unless @blog.user == current_user
   end
 
   def blog_params
