c54b0139fc1f2048cfb248f3c362f0a9cdcfd8e2
fkmr59 <f4k0m6r0@icloud.com>
2023-04-13 14:17:33 +0900

    xss対策のhelperメソッドの修正

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module BlogsHelper
-  include ActionView::Helpers::SanitizeHelper
-
   def format_content(blog)
-    sanitize(blog.content, tags: %w(br), attributes: %w())
+    blog_content = h(blog.content).gsub("\n", '<br>')
+    safe_tags = %w[br]
+    sanitize(blog_content, tags: safe_tags, attributes: [], encode_special_chars: true)
   end
 end
